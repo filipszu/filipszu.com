01729cf756c751886ba55cef40f678aec10e9ece
Filip Szulczewski <filipszu@gmail.com>
2021-03-01 22:17:01 -0500

    Fixed JSX in MainCard

--- code/src/components/MainCard/MainCard.js
+++ code/src/components/MainCard/MainCard.js
@@ -7,11 +7,12 @@ const mainCard = (props) => {
             <div className={classes.Logo}>
 					<h1>FiliPSZU</h1>
 					<h4>Software Engineer</h4>
-
-                    <footer>
-                            |&nbsp;<a target="_blank" href="https://twitter.com/filipszu">@filipszu</a>&nbsp;|&nbsp;<a target="_blank" href="https://github.com/filipszu">GitHub</a>&nbsp;|&nbsp;<a target="_blank" href="http://www.linkedin.com/in/filipszu">LinkedIn</a>&nbsp;|
-                    </footer>
 			</div>
+            <footer>
+                |&nbsp;<a target="_blank" href="https://twitter.com/filipszu">@filipszu</a>&nbsp;
+                |&nbsp;<a target="_blank" href="https://github.com/filipszu">GitHub</a>&nbsp;
+                |&nbsp;<a target="_blank" href="http://www.linkedin.com/in/filipszu">LinkedIn</a>&nbsp;|
+            </footer>
         </div>
     );
 };
