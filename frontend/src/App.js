d831675c8fb1542180d33746e1012bd1b9403a56
Filip Szulczewski <filipszu@gmail.com>
2021-03-28 14:38:22 -0400

    Style changes for mobile

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,11 @@ import classes from './App.module.css';
 import MainCard from './components/MainCard/MainCard';
 import WordCloud from './components/WordCloud/WordCloud';
 
-import {MobileView, BrowserView} from 'react-device-detect';
+import {BrowserView} from 'react-device-detect';
 
 function App() {
   return (
     <div className={classes.App}>
-      <div className={classes.BackDrop}></div>
       <BrowserView>
         <WordCloud delay={500} interval={30} wordMultiplier={15}/>
       </BrowserView>
