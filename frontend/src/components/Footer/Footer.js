dfed7357492daa45aa02dade6e158a837ea7f370
Filip Szulczewski <filipszu@gmail.com>
2021-03-31 14:40:24 -0400

    Fixing footer on tablet

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -1,22 +1,31 @@
 import classes from "./Footer.module.css";
-import { BrowserView, MobileView } from 'react-device-detect';
+import { BrowserView, isMobile, isTablet, MobileOnlyView } from 'react-device-detect';
 
 const footer = (props) => {
-    return (
-        <footer className={classes.Footer}>
-            <BrowserView>
-                |&nbsp;<a target="_blank" rel="nofollow noopener noreferrer" href="https://twitter.com/filipszu">@filipszu</a>&nbsp;
-                |&nbsp;<a target="_blank" rel="nofollow noopener noreferrer" href="https://github.com/filipszu">GitHub</a>&nbsp;
-                |&nbsp;<a target="_blank" rel="nofollow noopener noreferrer" href="https://stackoverflow.com/users/9680496/filip-szulczewski">StackOverflow</a>&nbsp;
-                |&nbsp;<a target="_blank" rel="nofollow noopener noreferrer" href="https://www.linkedin.com/in/filipszu">LinkedIn</a>&nbsp;|
-            </BrowserView>
-            <MobileView>
+    let links = (
+        <div>
+            |&nbsp;<a target="_blank" rel="nofollow noopener noreferrer" href="https://twitter.com/filipszu">@filipszu</a>&nbsp;
+            |&nbsp;<a target="_blank" rel="nofollow noopener noreferrer" href="https://github.com/filipszu">GitHub</a>&nbsp;
+            |&nbsp;<a target="_blank" rel="nofollow noopener noreferrer" href="https://stackoverflow.com/users/9680496/filip-szulczewski">StackOverflow</a>&nbsp;
+            |&nbsp;<a target="_blank" rel="nofollow noopener noreferrer" href="https://www.linkedin.com/in/filipszu">LinkedIn</a>&nbsp;|
+        </div>
+    );
+
+    if(isMobile || isTablet){
+        links = (
+            <div>
                 |&nbsp;<a target="_blank" rel="nofollow noopener noreferrer" href="https://twitter.com/filipszu">@filipszu</a>&nbsp;
                 |&nbsp;<a target="_blank" rel="nofollow noopener noreferrer" href="https://github.com/filipszu">GitHub</a>&nbsp;|
                 <br />
                 |&nbsp;<a target="_blank" rel="nofollow noopener noreferrer" href="https://stackoverflow.com/users/9680496/filip-szulczewski">StackOverflow</a>&nbsp;
                 |&nbsp;<a target="_blank" rel="nofollow noopener noreferrer" href="https://www.linkedin.com/in/filipszu">LinkedIn</a>&nbsp;|
-            </MobileView>
+            </div>
+        );
+    }
+
+    return (
+        <footer className={classes.Footer}>
+            {links}
         </footer>
     );
 };
