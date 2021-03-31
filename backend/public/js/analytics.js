b0716e9801ab28a060b72c3b57dd22989c4ca8b9
Filip Szulczewski <filipszu@gmail.com>
2021-03-31 12:07:21 -0400

    missing parentheses

--- backend/public/js/analytics.js
+++ backend/public/js/analytics.js
@@ -1,7 +1,7 @@
-function(){
+(function(){
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
 
     gtag('config', 'G-TZWNB1NLRN');
-}();
+})();
