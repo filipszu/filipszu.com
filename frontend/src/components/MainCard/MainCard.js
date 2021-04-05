a1996f2990e0de031af98dbdb2e4975816c56bea
Filip Szulczewski <filipszu@gmail.com>
2021-04-05 18:19:43 -0400

    Text update, grammar mainly.

--- frontend/src/components/MainCard/MainCard.js
+++ frontend/src/components/MainCard/MainCard.js
@@ -8,9 +8,9 @@ const MainCard = (props) => {
     const [aboutText, setAboutText] = useState(`Hi,<br />
         my name is <span class="${classes.TextWhite}">Filip Szulczewski</span>.<br />
         I’m an <span class="${classes.TextGreen}">autodidact programmer</span>, with <span class="${classes.TextWhite}">more than a decade of experience</span>.<br /><br />  
-        I’ve <span class="${classes.TextGreen}">architected</span>, <span class="${classes.TextGreen}">built</span> and <span class="${classes.TextGreen}">delivered</span> an array of <span class="${classes.TextPurple}">video applications</span> using <span class="${classes.TextPurple}">web technologies</span>. 
+        I’ve <span class="${classes.TextGreen}">architected</span>, <span class="${classes.TextGreen}">built</span>, and <span class="${classes.TextGreen}">delivered</span> an array of <span class="${classes.TextPurple}">video applications</span> using <span class="${classes.TextPurple}">web technologies</span>. 
         I have a <span class="${classes.TextWhite}">track record</span> of <span class="${classes.TextWhite}">succesfully augmenting teams</span> in <span class="${classes.TextWhite}">Professional Services</span> capacity.  
-        I have experience in working both on the <span class="${classes.TextPurple}">frontend</span> and the <span class="${classes.TextPurple}">backend</span>.  
+        I've worked both on <span class="${classes.TextPurple}">frontend</span> and <span class="${classes.TextPurple}">backend</span> projects.  
         I’m a <span class="${classes.TextPurple}">FOSS</span> enthusiast with a <span class="${classes.TextPurple}">DIY</span> spirit.`);
 
     return (
