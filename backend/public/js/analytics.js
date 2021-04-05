a985f5d33737149e03a7436bcd0c17a18203ff26
Filip Szulczewski <filipszu@gmail.com>
2021-04-05 18:20:58 -0400

    preventing analytics for localhost. BE

--- backend/public/js/analytics.js
+++ backend/public/js/analytics.js
@@ -1,7 +1,9 @@
 (function(){
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-
-    gtag('config', 'G-TZWNB1NLRN');
+    var host = window.location.hostname;
+    if(host != "localhost"){
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-TZWNB1NLRN');    
+    }
 })();
