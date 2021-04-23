823499db439660e53df40d5a9ed377bac2221d09
Filip Szulczewski <filipszu@gmail.com>
2021-04-23 18:42:23 -0400

    Using the custom Font. Removing not needed style files.

--- v2/pages/index.tsx
+++ v2/pages/index.tsx
@@ -1,14 +1,12 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-
 import Home from '../home/Home';
 
 export default function HomePage() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Filip Szulczewski - Software Engineer</title>
-        <link rel="icon" href="/favicon.png" />
+        <link rel="icon" href="/assets/favicon.png" />
       </Head>
       <Home />
     </div>
