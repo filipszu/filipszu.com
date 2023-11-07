ee8cff6f51f2055e94959a460bcc916527b6d92d
filipszu <filipszu@gmail.com>
2023-11-07 12:44:46 -0500

    changed the implementation to actualy cancelAnimationFrame updated comments

--- frontend/src/components/Home/hooks/useAnimationFrame.ts
+++ frontend/src/components/Home/hooks/useAnimationFrame.ts
@@ -2,20 +2,20 @@ import { useCallback, useEffect, useRef } from 'react';
 
 /**
  * **The callback** that will be called. 
- * Similair to `FrameRequestCallback` from `requestAnimationFrame`  
+ * Similar to `FrameRequestCallback` from `requestAnimationFrame`  
  * It is intended to perform some logic based on time i.e. an animation.  
- * The returned `number` will be the timeout in miliseconds before next time **the callback** function gets called.  
+ * The returned `number` will be the timeout in milliseconds before next time **the callback** function gets called.  
  * Returning `0` will stop calling **the callback** and cause `cancelAnimationFrame` to be called under the hood.
  * @param time The DOMHighResTimeStamp from requestAnimationFrame.
- * @return The number of miliseconds before the next call.
+ * @return The number of milliseconds before the next call.
  */
 export type UseAnimationFrameCallback = (time: DOMHighResTimeStamp) => number;
 
 /**
  * A hook wrapper around `requestAnimationFrame`.  
- * Accepts an `UseAnimationFrameCallback` as an argument.  
- * Similair to `FrameRequestCallback` will be called before the browser performs a repaint.  
- * The `number` returned from **the callback** will be the amount of miliseconds before the next callback call.
+ * Accepts a `UseAnimationFrameCallback` as an argument.  
+ * Similar to `FrameRequestCallback` will be called before the browser performs a repaint.  
+ * The `number` returned from **the callback** will be the amount of milliseconds before the next callback call.
  * 
  * **Example of use:**  
  * * If you want an animation to run every 25ms keep returning 25 from **the callback**.  
@@ -38,18 +38,22 @@ const useAnimationFrame = (callback: UseAnimationFrameCallback) => {
     const requestRef = useRef(0);
     const nextTickTimeRef = useRef(0);
     
+    // This function is passed to requestAnimationFrame. It calls the callback and schedules the next animation frame.
     const animate: FrameRequestCallback = useCallback(time => {
         if(!nextTickTimeRef.current || time >= nextTickTimeRef.current){
-            nextTickTimeRef.current = time + callback(time);
+            const delay = callback(time);
+            nextTickTimeRef.current = delay > 0 ? time + delay : 0;
         }
 
-        if(!nextTickTimeRef.current || nextTickTimeRef.current > time){
+        if(nextTickTimeRef.current >= time){
             requestRef.current = requestAnimationFrame(animate);
+        } else {
+            cancelAnimationFrame(requestRef.current);
         }
     }, [callback]);
 
     useEffect(() => {
-        // requestRef.current = requestAnimationFrame(animate);
+        // Call animate immediately when the component mounts
         animate(0);
         return () => cancelAnimationFrame(requestRef.current);
     }, [animate]);
